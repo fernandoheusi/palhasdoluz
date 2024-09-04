a76496ad9291be17732b58872ffe99f691cbd94f
fernandoheusi <fernandoj.mhs@gmail.com>
2024-09-04 13:10:24 -0400

    unfix header

--- src/app/mock/pontosDeVenda.ts
+++ src/app/mock/pontosDeVenda.ts
@@ -19,73 +19,81 @@ export const sellingPoints = [
 		name: 'Comitiva do Chope',
 		coordenates: {
 			latitude: -20.4796253,
-			longitude: -54.585921,
+			longitude: -54.583507,
 		},
-		googleMapsUrl: null,
+		googleMapsUrl: 'https://maps.app.goo.gl/ce666bLQU8LN2wtA8',
 	},
 	{
 		name: 'Xadrez',
 		coordenates: {
-			latitude: -20.4741392,
-			longitude: -54.6904484,
+			latitude: -20.4818123,
+			longitude: -54.6614441,
 		},
-		googleMapsUrl: null,
+		googleMapsUrl: 'https://maps.app.goo.gl/bt3ygznk3FETAHQLA',
 	},
 	{
 		name: 'Da hora',
 		coordenates: {
-			latitude: -20.4741295,
-			longitude: -54.6904485,
+			latitude: -20.4893785,
+			longitude: -54.6699073,
 		},
-		googleMapsUrl: null,
+		googleMapsUrl: 'https://maps.app.goo.gl/JAXimcyxnr6jEFCq9',
 	},
 	{
 		name: 'Extrabom',
 		coordenates: {
-			latitude: -20.4834000,
-			longitude: -54.6691000,
+			latitude: -20.483594,
+			longitude: -54.6695997,
 		},
-		googleMapsUrl: null,
+		googleMapsUrl: 'https://maps.app.goo.gl/Z2nCqGaMtCV9Zkwc7',
 	},
 	{
 		name: 'Excelência',
 		coordenates: {
-			latitude: null,
-			longitude: null,
+			latitude: -20.4803664,
+			longitude: -54.5795203,
 		},
-		googleMapsUrl: null,
+	googleMapsUrl: "https://www.google.com/maps/place/Av.+Jos%C3%A9+Nogueira+Vi%C3%AAira,+464+-+Jardim+Sao+Lourenco,+Campo+Grande+-+MS,+79042-010/@-20.4803664,-54.5795203,21z/data=!4m15!1m8!3m7!1s0x9486ef33ad35e631:0x2c5db550c720a9bb!2sAv.+Jos%C3%A9+Nogueira+Vi%C3%AAira,+464+-+Jardim+Sao+Lourenco,+Campo+Grande+-+MS,+79042-010!3b1!8m2!3d-20.4803182!4d-54.5793609!16s%2Fg%2F11f03n5jry!3m5!1s0x9486ef33ad35e631:0x2c5db550c720a9bb!8m2!3d-20.4803182!4d-54.5793609!16s%2Fg%2F11f03n5jry?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D",
 	},
 	{
 		name: 'Milena',
 		coordenates: {
-			latitude: null,
-			longitude: null,
+			latitude: -20.4865465,
+			longitude: -54.651152,
 		},
-		googleMapsUrl: null,
+	googleMapsUrl: 'https://www.google.com/maps/place/Milena+Conveni%C3%AAncia/@-20.4865465,-54.651152,21z/data=!4m15!1m8!3m7!1s0x9486e67dac0395af:0xb5c0731f4d8a6600!2sR.+Albert+Sabin,+1953+-+Vila+Taveiropolis,+Campo+Grande+-+MS,+79090-160!3b1!8m2!3d-20.4865724!4d-54.6510043!16s%2Fg%2F11csl3xb9j!3m5!1s0x9486e67dabd59069:0xf5de8ff46b958c2c!8m2!3d-20.4864868!4d-54.6509851!16s%2Fg%2F11c2npq_h8?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
 	},
 	{
 		name: 'Up Beer',
 		coordenates: {
-			latitude: -20.4702542,
-			longitude: -54.6378693,
+			latitude: -20.4752538,
+			longitude: -54.5832435,
 		},
-		googleMapsUrl: null,
+		googleMapsUrl: 'https://maps.app.goo.gl/9QeWZLykFCrAYMmV9',
 	},
 	{
 		name: 'Big',
 		coordenates: {
-			latitude: null,
-			longitude: null,
+			latitude: -20.5021081,
+			longitude: -54.6717663,
 		},
-		googleMapsUrl: null,
+		googleMapsUrl: 'https://www.google.com/maps/@-20.5021081,-54.6717663,21z?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
 	},
 	{
 		name: 'Escova',
 		coordenates: {
-			latitude: -20.4935713,
-			longitude: -54.6957601,
+			latitude: -20.4985854,
+			longitude: -54.6820183,
 		},
-		googleMapsUrl: null,
+		googleMapsUrl: 'https://maps.app.goo.gl/jzziWEgGX29UZNKX6',
+	},
+	{
+		name: 'Restaurante Esplendido',
+		coordenates: {
+			latitude: -20.4356493,
+			longitude: -54.5698373,
+		},
+		googleMapsUrl: 'https://www.google.com/maps/place/Espl%C3%AAndido+Restaurante+%EF%B8%8F/@-20.4356493,-54.5698373,21z/data=!4m6!3m5!1s0x9486e94142c58763:0x3e555a942431f974!8m2!3d-20.4356218!4d-54.5697464!16s%2Fg%2F11v07jptbt?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
 	},
 	{
 		name: 'JR Mercado',
@@ -98,66 +106,162 @@ export const sellingPoints = [
 	{
 		name: 'Tendas',
 		coordenates: {
-			latitude: -20.4824421,
-			longitude: -54.6624973,
+			latitude: -20.481966,
+			longitude: -54.6617184,
 		},
-		googleMapsUrl: null,
+		googleMapsUrl: 'https://maps.app.goo.gl/h73o6M6tEnV9gYoq6',
 	},
 	{
-		name: 'Camargo',
+		name: 'Barraca da Tania',
 		coordenates: {
-			latitude: -20.4827808,
-			longitude: -54.6731932,
+			latitude: -20.4704869,
+			longitude: -20.4704869,
 		},
-		googleMapsUrl: null,
+		googleMapsUrl: 'https://www.google.com/maps/place/Av.+Rodolfo+Jos%C3%A9+Pinho,+95+-+Jardim+Sao+Bento,+Campo+Grande+-+MS,+79004-372/@-20.4704869,-54.6070485,21z/data=!4m15!1m8!3m7!1s0x9486e8a701626eb5:0x805589caae6d81a4!2sAv.+Rodolfo+Jos%C3%A9+Pinho,+95+-+Jardim+Sao+Bento,+Campo+Grande+-+MS,+79004-372!3b1!8m2!3d-20.4704567!4d-54.6069036!16s%2Fg%2F11c4kp9w9h!3m5!1s0x9486e8a701626eb5:0x805589caae6d81a4!8m2!3d-20.4704567!4d-54.6069036!16s%2Fg%2F11c4kp9w9h?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
 	},
 	{
-		name: 'Tex HOOKA',
+		name: "Camargo's",
 		coordenates: {
-			latitude: -20.4718858,
-			longitude: -54.6454757,
+			latitude: -20.4826972,
+			longitude: -54.6624324,
 		},
-		googleMapsUrl: null,
+		googleMapsUrl: 'https://maps.app.goo.gl/tY2omdQXzah7oQKk8',
 	},
+	// {
+	// 	name: 'Tex HOOKA',
+	// 	coordenates: {
+	// 		latitude: -20.4718723,
+	// 		longitude: -54.6429909,
+	// 	},
+	// 	googleMapsUrl: 'https://maps.app.goo.gl/S3EQJX75WEztbnxY9',
+	// },
 	{
 		name: 'Morumbi Beer',
 		coordenates: {
-			latitude: -20.4898292,
-			longitude: -54.6062172,
+			latitude: -20.5013053,
+			longitude: -54.5920201,
 		},
-		googleMapsUrl: null,
+		googleMapsUrl: 'https://maps.app.goo.gl/JTZXj2D2PorEk3wg7',
 	},
 	{
 		name: 'Gelados',
 		coordenates: {
-			latitude: null,
-			longitude: null,
+			latitude: -20.4963612,
+			longitude: -54.5886169,
 		},
-		googleMapsUrl: null,
+		googleMapsUrl: 'https://www.google.com/maps/place/Gelados+Conveni%C3%AAncia/@-20.4963612,-54.5886169,21z/data=!4m15!1m8!3m7!1s0x9486ef4090d68cf3:0x179be484cf51d9cc!2sAv.+Toros+Puxian,+963+-+Vila+Morumbi,+Campo+Grande+-+MS,+79052-030!3b1!8m2!3d-20.4963975!4d-54.5884957!16s%2Fg%2F11sj9mvl6j!3m5!1s0x9486ef11511dbfdb:0x502e29e4c790fd0b!8m2!3d-20.4963975!4d-54.5884957!16s%2Fg%2F11tnm1v1gk?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
 	},
 	{
 		name: 'Casa do ovo',
 		coordenates: {
-			latitude: null,
-			longitude: null,
+			latitude: -20.4372342,
+			longitude: -54.6559324,
 		},
-		googleMapsUrl: null,
+		googleMapsUrl: 'https://www.google.com/maps/place/Central+Do+Ovo(distribuidora)/@-20.4372342,-54.6559324,21z/data=!4m15!1m8!3m7!1s0x9486e70a35dd97fd:0x1ca781d8a7b35b40!2sR.+Yokoama,+1073+-+Vila+Santo+Amaro,+Campo+Grande+-+MS,+79112-260!3b1!8m2!3d-20.4372387!4d-54.6557964!16s%2Fg%2F11s4syg12_!3m5!1s0x9486e7c27a273225:0x380843ddef2eb16d!8m2!3d-20.4372862!4d-54.6558714!16s%2Fg%2F11sdzp860d?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
 	},
 	{
-		name: 'Amigão',
+		name: '46 Lanches',
 		coordenates: {
-			latitude: -20.4930085,
-			longitude: -54.7230606,
+			latitude: -20.4936362,
+			longitude: -54.6715856,
 		},
-		googleMapsUrl: null,
+		googleMapsUrl: 'https://www.google.com/maps/place/46+Lanches/@-20.4936362,-54.6715856,21z/data=!4m15!1m8!3m7!1s0x9486e69ecc80c8a3:0x786a33e8b2f5a3c6!2sR.+Ant%C3%B4nio+Bandeira,+614+-+Nucleo+Hab.+Buriti,+Campo+Grande+-+MS,+79091-060!3b1!8m2!3d-20.4936672!4d-54.6715693!16s%2Fg%2F11k3_t_wj3!3m5!1s0x9486e7d65ee7f7a7:0xc06d0404e547b6a3!8m2!3d-20.4936775!4d-54.6714929!16s%2Fg%2F11h88l2_xg?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
+	},
+	// {
+	// 	name: 'Amigão',
+	// 	coordenates: {
+	// 		latitude: -20.4903458,
+	// 		longitude: -54.6673498,
+	// 	},
+	// 	googleMapsUrl: 'https://maps.app.goo.gl/c6Ejw6gkKeKHrhHN9',
+	// },
+	{
+		name: 'Gordinho da coxinha',
+		coordenates: {
+			latitude: -20.4819708,
+			longitude: -54.6586962,
+		},
+		googleMapsUrl: 'https://www.google.com/maps/place/Gordinho+da+Coxinha/@-20.4819708,-54.6586962,21z/data=!4m14!1m7!3m6!1s0x9486e78bbc61ddc5:0xce7f0d09984e0d13!2sGordinho+da+Coxinha!8m2!3d-20.4820967!4d-54.6586903!16s%2Fg%2F11vcwl8jd1!3m5!1s0x9486e78bbc61ddc5:0xce7f0d09984e0d13!8m2!3d-20.4820967!4d-54.6586903!16s%2Fg%2F11vcwl8jd1?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
 	},
 	{
-		name: 'Gordinho',
+		name: 'Gordinho da coxinha',
 		coordenates: {
-			latitude: null,
-			longitude: null,
+			latitude: -20.4819708,
+			longitude: -54.6586962,
 		},
-		googleMapsUrl: null,
+		googleMapsUrl: 'https://www.google.com/maps/place/Gordinho+da+Coxinha/@-20.4819708,-54.6586962,21z/data=!4m14!1m7!3m6!1s0x9486e78bbc61ddc5:0xce7f0d09984e0d13!2sGordinho+da+Coxinha!8m2!3d-20.4820967!4d-54.6586903!16s%2Fg%2F11vcwl8jd1!3m5!1s0x9486e78bbc61ddc5:0xce7f0d09984e0d13!8m2!3d-20.4820967!4d-54.6586903!16s%2Fg%2F11vcwl8jd1?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
+	},
+	{
+		name: 'Pedrão Beer',
+		coordenates: {
+			latitude: -20.4894769,
+			longitude: -54.5925863,
+		},
+		googleMapsUrl: 'https://www.google.com/maps/place/Pedr%C3%A3o+Beer+Delivery+%7C+Conveni%C3%AAncia+em+Campo+Grande+MS./@-20.4894769,-54.5925863,21z/data=!4m10!1m2!2m1!1sPedr%C3%A3o+Beer!3m6!1s0x9486ef4b839e881d:0xf8338c937fad30f5!8m2!3d-20.48957!4d-54.592316!15sCgxQZWRyw6NvIEJlZXKSARFiZXZlcmFnZV9zdXBwbGllcuABAA!16s%2Fg%2F11rb2vr3hl?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
+	},
+	{
+		name: 'Bar bamboa',
+		coordenates: {
+			latitude: -20.4973857,
+			longitude: -54.608615,
+		},
+		googleMapsUrl: 'https://www.google.com/maps/place/Conveni%C3%AAncia+Bar+Escobar/@-20.4973857,-54.608615,21z/data=!4m15!1m8!3m7!1s0x9486ef588dec6693:0xe6f60f5264bc5f31!2sR.+Montese,+264+-+Vila+Olinda,+Campo+Grande+-+MS,+79060-100!3b1!8m2!3d-20.4973604!4d-54.6084458!16s%2Fg%2F11c5q05bg1!3m5!1s0x9486ef588ae28dab:0xcd1dda2ed7ac9021!8m2!3d-20.4973902!4d-54.6084444!16s%2Fg%2F1tdvwddq?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
+	},
+	{
+		name: 'Multi Mercadinho',
+		coordenates: {
+			latitude: -20.4693671,
+			longitude: -54.6453392,
+		},
+		googleMapsUrl: 'https://www.google.com/maps/place/Mult+Mercadinho/@-20.4693671,-54.6453392,21z/data=!4m15!1m8!3m7!1s0x9486e65d001f93f1:0xec8254b7b577d6db!2sR.+Rodolfo+Andrade+Pinho,+550+-+Vila+Taveiropolis,+Campo+Grande+-+MS,+79090-050!3b1!8m2!3d-20.46935!4d-54.6450579!16s%2Fg%2F11ggnd0s6r!3m5!1s0x9486e7007115526f:0x1309a470ac4a0ee9!8m2!3d-20.46935!4d-54.6450579!16s%2Fg%2F11vt3zykrg?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
+	},
+	{
+		name: 'Conveniência Santo Point',
+		coordenates: {
+			latitude: -20.499174,
+			longitude: -54.582831,
+		},
+		googleMapsUrl: 'https://www.google.com/maps/place/Consulado+Oficial+Palmeiras+MS/@-20.499174,-54.582831,21z/data=!4m15!1m8!3m7!1s0x9486ef15e538d97d:0xe481f21947783903!2sAv.+Rita+Vieira+de+Andrade,+1080+-+Jardim+Mansur,+Campo+Grande+-+MS,+79052-420!3b1!8m2!3d-20.4992047!4d-54.5826819!16s%2Fg%2F11frlsc96p!3m5!1s0x9486ef3a02a69537:0x8b75f187a2fbd3a5!8m2!3d-20.4992047!4d-54.5826819!16s%2Fg%2F11t89d6zpk?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
+	},
+	{
+		name: 'Espaço Vera',
+		coordenates: {
+			latitude: -20.4512534,
+			longitude: -54.6445887,
+		},
+		googleMapsUrl: 'https://www.google.com/maps/place/Av.+dos+Cris%C3%A2ntemos,+353+-+Lar+do+Trab.,+Campo+Grande+-+MS,+79110-580/@-20.4512534,-54.6445887,21z/data=!4m6!3m5!1s0x9486e652cd50bf3d:0x25602c7ad12578c9!8m2!3d-20.4512318!4d-54.6444384!16s%2Fg%2F11f3m8d9t9?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
+	},
+	{
+		name: 'Espetinho da Ruth',
+		coordenates: {
+			latitude: -20.4805474,
+			longitude: -54.6643578,
+		},
+		googleMapsUrl: 'https://www.google.com/maps/place/Espetinho+da+Ruth/@-20.4805474,-54.6643578,21z/data=!4m14!1m7!3m6!1s0x9486e73ef512294b:0x4c94e5897520fef!2sEspetinho+da+Ruth!8m2!3d-20.4806192!4d-54.664241!16s%2Fg%2F11nxfqpjlh!3m5!1s0x9486e73ef512294b:0x4c94e5897520fef!8m2!3d-20.4806192!4d-54.664241!16s%2Fg%2F11nxfqpjlh?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
+	},
+	{
+		name: 'Conveniencia do Gaeta',
+		coordenates: {
+			latitude: -20.4387054,
+			longitude: -54.5712189,
+		},
+		googleMapsUrl: 'https://www.google.com/maps/place/Conveni%C3%AAncia+do+Gaeta/@-20.4387054,-54.5712189,21z/data=!4m10!1m2!2m1!1sConveniencia+do+Gaeta!3m6!1s0x9486e90027a73ba5:0xe7f1410586689070!8m2!3d-20.4387903!4d-54.5711631!15sChVDb252ZW5pZW5jaWEgZG8gR2FldGGSAQxsaXF1b3Jfc3RvcmXgAQA!16s%2Fg%2F11y405dyr0?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
+	},
+	{
+		name: 'Jota Beer',
+		coordenates: {
+			latitude: -20.4339773,
+			longitude: -54.5704283,
+		},
+		googleMapsUrl: 'https://www.google.com/maps/place/Jota+Beer+Conveni%C3%AAncia/@-20.4339773,-54.5704283,21z/data=!4m14!1m7!3m6!1s0x9486e9f90c836b55:0xa6a0809b181ec3b7!2sJota+Beer+Conveni%C3%AAncia!8m2!3d-20.4339142!4d-54.5703597!16s%2Fg%2F11q46rtm5x!3m5!1s0x9486e9f90c836b55:0xa6a0809b181ec3b7!8m2!3d-20.4339142!4d-54.5703597!16s%2Fg%2F11q46rtm5x?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
+	},
+	{
+		name: 'Pastelandia',
+		coordenates: {
+			latitude: -20.4218549,
+			longitude: -54.6363158,
+		},
+		googleMapsUrl: 'https://www.google.com/maps/place/Pastel%C3%A2ndia+-++Pastelaria+Gourmet/@-20.4218549,-54.6363158,20.66z/data=!4m5!3m4!1s0x9486e7d63b64ffe7:0xe08bd210fc723dcb!8m2!3d-20.4218072!4d-54.6363804?entry=ttu&g_ep=EgoyMDI0MDgyMC4xIKXMDSoASAFQAw%3D%3D',
 	},
 ]
 
--- src/app/pages/pontos-de-venda/pontos-de-venda.component.ts
+++ src/app/pages/pontos-de-venda/pontos-de-venda.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Inject, Input } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { ProductInterface } from '../../models/Product';
 import { ProductsService } from '../../services/products';
 import { HeaderComponent } from '../../components/header/header.component';
@@ -18,6 +19,7 @@ import { SellingPointsInterface, sellingPoints } from '../../mock/pontosDeVenda'
 export class PontosDeVendaComponent implements AfterViewInit {
   private map:L.Map;
   private centroid:L.LatLngExpression= [-20.4810998, -54.635534 ];
+  @Inject(DOCUMENT) private document: Document
 
   private onClick():void{
     console.log('oi')
@@ -26,7 +28,7 @@ export class PontosDeVendaComponent implements AfterViewInit {
   icon = {
   icon: new L.DivIcon({
     className: 'my-div-icon',
-    html: '<img class="my-div-image" src="../assets/palhas_icon.png" width="25px"/>'+
+    html: '<img class="my-div-image" src="../assets/palhas_icon.png" width="25px" height="25px"/>'+
           '<span class="my-div-span">RAF Banff Airfield</span>'
     })
   };
@@ -51,26 +53,41 @@ export class PontosDeVendaComponent implements AfterViewInit {
       const icon = {
         icon: new L.DivIcon({
           className: 'my-div-icon',
-          html: '<img class="my-div-image" src="../assets/palhas_icon.png" width="25px"/>'+
-                `<p class="my-div-span">${sellingPoint.name}</p>`
+          html: `
+                <div class="icon-div">
+                  <img href=${sellingPoint.googleMapsUrl} class="my-div-image" src="../assets/palhas_icon.png" width="25px" height="25px"/>
+                  <p class="my-div-span">${sellingPoint.name}</p>
+                </div>
+                `
           })
         };
       return icon
   };
+  function goToUrl(sellingPoint: SellingPointsInterface):()=>void {
+    return function (){window.open(
+      `${sellingPoint.googleMapsUrl}`,
+      '_blank' 
+    );}
+  }
+  
+  function getOnClick(sellingPoint: SellingPointsInterface):()=>void{
+    return goToUrl(sellingPoint)
+  }
+  
+  sellingPoints.forEach(sellingPoint => {
+    // L.marker([sellingPoint.coordenates.latitude ?? 0,sellingPoint.coordenates.longitude ?? 0], getIcon(sellingPoint)).addTo(this.map).on('click', this.onClick);
+    const correctedLatitude = (sellingPoint.coordenates.latitude ?? 0) - 0
+    const correctedLongitude = (sellingPoint.coordenates.longitude ?? 0) + 0.00
+    // L.marker([correctedLatitude,correctedLongitude], getIcon(sellingPoint)).addTo(this.map).on('click', getOnClick(sellingPoint)).bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    L.marker([correctedLatitude,correctedLongitude], getIcon(sellingPoint)).addTo(this.map).bindPopup(`<a href=${sellingPoint.googleMapsUrl} target=_blank>${sellingPoint.name}</a>`)
+  })
+  
+  tiles.addTo(this.map);
+}
 
-  function getOnClick(){
-    
+  constructor(private productsService: ProductsService,) {
   }
 
-    sellingPoints.forEach(sellingPoint => {
-      L.marker([sellingPoint.coordenates.latitude ?? 0,sellingPoint.coordenates.longitude ?? 0], getIcon(sellingPoint)).addTo(this.map).on('click', this.onClick);
-    })
-
-    tiles.addTo(this.map);
-  }
-
-  constructor(private productsService: ProductsService) {
-  }
 
   ngAfterViewInit(): void {
     this.initMap();
